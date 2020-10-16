af6407853ef59ac98a6f6391204ff1606870b5e7
Shreyas Pant <shrey.pant88@gmail.com>
2020-10-16 14:18:19 -0400

    Finishing up Twitter

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -20,7 +20,21 @@ class TweetCell: UITableViewCell {
     var tweetId: Int = -1
     
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("error \(error)")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted: Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     @IBAction func favorite(_ sender: Any) {
         let toBeFav = !favorited
